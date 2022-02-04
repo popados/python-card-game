94124c08f40d333a92ca8aa08ea0262e2807b97a
popados <popados@gmail.com>
2022-02-03 23:40:12 -0600

    second day

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Day 1 | 2/2/2022 - Wednesday - /*my dude*/
 
 - print statements & variables & operators & string manipulations
 
+Day 2 | 2/3/2022 - Thursday -
+
+- Actually did what I said I was gunna do yesterday.
--- output.py
+++ output.py
@@ -1,4 +1,17 @@
 
+x = input("enter a number \n")
+
+y = input ("enter a second number \n")
+
+z = 0
+def add_nums(x,y) :
+    print('first number :', x)
+    print('second number :', y)
+    z = int(x) + int(y)
+    print(z)
+
+add_nums(x, y)
+
 
 
 hi = 'hello'
